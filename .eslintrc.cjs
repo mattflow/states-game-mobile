9f156a1c2ab2a3536042f9e3a6d128de276519ca
Matt Flowers <mattmflowers28@gmail.com>
2023-06-05 02:50:10 -0400

    Extend linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,11 @@
 module.exports = {
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-native/all",
+  ],
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "react", "react-native"],
   root: true,
   ignorePatterns: [".eslintrc.cjs", "babel.config.js"],
 };
